4d9c4b5caba81723c6847a9f1506038c4f88ebed
alexander.kholodilov <a.kholodilov@finanso.com>
2021-11-11 00:04:51 +0300

    update

--- mkconfig.sh
+++ mkconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir -p /etc/google-cloud-ops-agent/
+rm -f /etc/google-cloud-ops-agent/config.yaml
 curl -o /etc/google-cloud-ops-agent/config.yaml https://raw.githubusercontent.com/megahertz00/gcloud-opsagent-custom/main/config.yaml
 sudo service google-cloud-ops-agent restart
