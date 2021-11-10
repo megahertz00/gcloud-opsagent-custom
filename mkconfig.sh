76d6efbc89bd932f156ac252c1b645203930adba
alexander.kholodilov <a.kholodilov@finanso.com>
2021-11-10 23:55:11 +0300

    first commit

--- mkconfig.sh
+++ mkconfig.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p /etc/google-cloud-ops-agent/
-curl -o /etc/google-cloud-ops-agent/config.yaml 
+curl -o /etc/google-cloud-ops-agent/config.yaml https://raw.githubusercontent.com/megahertz00/gcloud-opsagent-custom/main/config.yaml
+systemctl restart google-cloud-ops-agent.service
